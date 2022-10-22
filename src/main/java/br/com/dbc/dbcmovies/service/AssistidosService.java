7e21db450f6fc8076a1153635064788fe1bdd2c8
Alison8547 <alisonailson100@hotmail.com>
2022-10-22 12:14:41 -0300

    Assistidos service

---
+++ src/main/java/br/com/dbc/dbcmovies/service/AssistidosService.java
@@ -0,0 +1,52 @@
+package br.com.dbc.dbcmovies.service;
+
+import br.com.dbc.dbcmovies.entity.ItemEntretenimento;
+import br.com.dbc.dbcmovies.exceptions.BancoDeDadosException;
+import br.com.dbc.dbcmovies.exceptions.RegraDeNegocioException;
+import br.com.dbc.dbcmovies.repository.AssistidosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AssistidosService {
+
+    private AssistidosRepository assistidosRepository;
+    private UsuarioService usuarioService;
+    private ItemService itemService;
+
+    public AssistidosService(AssistidosRepository assistidosRepository, UsuarioService usuarioService, ItemService itemService) {
+        this.assistidosRepository = assistidosRepository;
+        this.usuarioService = usuarioService;
+        this.itemService = itemService;
+    }
+
+    public List<ItemEntretenimento> listarAssistidos(Integer idUsuario) throws BancoDeDadosException, RegraDeNegocioException {
+        usuarioService.findById(idUsuario);
+        return assistidosRepository.listarAssistidos(idUsuario);
+    }
+
+    public void deletarAssistido(Integer idItem, Integer idUsuario) throws BancoDeDadosException, RegraDeNegocioException {
+        itemService.findById(idItem);
+        usuarioService.findById(idUsuario);
+        assistidosRepository.deletarAssistido(idItem, idUsuario);
+    }
+
+    public ItemEntretenimento marcarAssistido(Integer idUsuario, Integer idItem) throws RegraDeNegocioException, BancoDeDadosException {
+        itemService.findById(idItem);
+        usuarioService.findById(idUsuario);
+
+       assistidosRepository.marcarAssistido(idUsuario,idItem);
+
+       return itemService.findById(idItem);
+
+
+
+    }
+
+    public void incluirIndicacao(String itemNome,Integer idUsuario) throws RegraDeNegocioException, BancoDeDadosException {
+        usuarioService.findById(idUsuario);
+        assistidosRepository.incluirIndicacao(itemNome,idUsuario);
+    }
+
+}
