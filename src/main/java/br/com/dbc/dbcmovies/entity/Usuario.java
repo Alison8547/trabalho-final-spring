94bf6ef184add8a211bf03d0cf172fc9b477b86e
Alison8547 <alisonailson100@hotmail.com>
2022-10-24 18:18:12 -0300

    tirando algumas validacoes

--- src/main/java/br/com/dbc/dbcmovies/entity/Usuario.java
+++ src/main/java/br/com/dbc/dbcmovies/entity/Usuario.java
@@ -5,22 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-
 @AllArgsConstructor
 @Getter
 @Setter
 public  class Usuario {
 
     private Integer id;
-    @NotNull
     private String nome;
-    @NotNull
     private Integer idade;
-    @Email
     private String email;
-    @NotNull
     private String senha;
 
     protected TipoUsuario tipoUsuario;
