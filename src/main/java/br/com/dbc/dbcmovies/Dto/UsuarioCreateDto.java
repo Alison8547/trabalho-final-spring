2196f7916bbe91a7f6d40f569e0f207e8e64c3c0
Alison8547 <alisonailson100@hotmail.com>
2022-10-26 09:08:08 -0300

    documentacao usuario

--- src/main/java/br/com/dbc/dbcmovies/Dto/UsuarioCreateDto.java
+++ src/main/java/br/com/dbc/dbcmovies/Dto/UsuarioCreateDto.java
@@ -1,6 +1,7 @@
 package br.com.dbc.dbcmovies.Dto;
 
 import br.com.dbc.dbcmovies.entity.TipoUsuario;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
@@ -9,13 +10,17 @@ import javax.validation.constraints.NotNull;
 @Data
 public class UsuarioCreateDto {
     @NotNull
+    @Schema(description = "Nome do usuário",example = "Alison")
     private String nome;
     @NotNull
+    @Schema(description = "Idade do usuário",example = "20")
     private Integer idade;
     @Email
+    @Schema(description = "Email do usuário",example = "alison@hotmail.com")
     private String email;
     @NotNull
+    @Schema(description = "Senha do usuário",example = "12345")
     private String senha;
-
+    @Schema(description = "Tipo do usuário", example = "CLIENTE")
     protected TipoUsuario tipoUsuario;
 }
--- src/main/java/br/com/dbc/dbcmovies/controller/UsuarioController.java
+++ src/main/java/br/com/dbc/dbcmovies/controller/UsuarioController.java
@@ -2,10 +2,12 @@ package br.com.dbc.dbcmovies.controller;
 
 import br.com.dbc.dbcmovies.Dto.UsuarioCreateDto;
 import br.com.dbc.dbcmovies.Dto.UsuarioDto;
-import br.com.dbc.dbcmovies.entity.Usuario;
-import br.com.dbc.dbcmovies.exceptions.BancoDeDadosException;
 import br.com.dbc.dbcmovies.exceptions.RegraDeNegocioException;
 import br.com.dbc.dbcmovies.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,49 +20,97 @@ import java.util.List;
 @Slf4j
 @RestController
 @Validated
+@RequiredArgsConstructor
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
 
+    @Operation(summary = "Listar Usuários", description = "Lista todos os usuários do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a lista de usuários"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<UsuarioDto>> listar() throws RegraDeNegocioException {
         return new ResponseEntity<>(usuarioService.listar(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Pega o usuário pelo id dele", description = "Resgata o usuário do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Foi resgatado com sucesso"),
+                    @ApiResponse(responseCode = "404", description = "Não encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping("/{idUsuario}")
-    public ResponseEntity<UsuarioDto> pegar(@PathVariable(name = "idUsuario")Integer idUsuario) throws  RegraDeNegocioException {
-        return new ResponseEntity<>(usuarioService.pegar(idUsuario),HttpStatus.OK);
+    public ResponseEntity<UsuarioDto> pegar(@PathVariable(name = "idUsuario") Integer idUsuario) throws RegraDeNegocioException {
+        return new ResponseEntity<>(usuarioService.pegar(idUsuario), HttpStatus.OK);
     }
 
+    @Operation(summary = "Criar usuário", description = "Cria um usuário no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Usuário Criado com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping
     public ResponseEntity<UsuarioDto> adicionar(@Valid @RequestBody UsuarioCreateDto usuario) throws RegraDeNegocioException {
         log.info("Adicionando o Usuário...");
         UsuarioDto user = usuarioService.adicionar(usuario);
         log.info("Usuário adicionado com sucesso!");
-        return new ResponseEntity<>(user,HttpStatus.CREATED);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Atualizar Usuário", description = "Atualiza o usuário no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Atualizou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PutMapping("/{idUsuario}")
-    public ResponseEntity<UsuarioDto> editar(@PathVariable(name = "idUsuario")Integer idUsuario,@Valid @RequestBody UsuarioCreateDto usuario) throws RegraDeNegocioException {
+    public ResponseEntity<UsuarioDto> editar(@PathVariable(name = "idUsuario") Integer idUsuario, @Valid @RequestBody UsuarioCreateDto usuario) throws RegraDeNegocioException {
         log.info("Editando o Usuário...");
         UsuarioDto user = usuarioService.editar(idUsuario, usuario);
         log.info("Usuário editado com sucesso!");
-        return new ResponseEntity<>(user,HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletar Usuário", description = "Deletar o usuário no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "204", description = "Deletado com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "404", description = "Não encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @DeleteMapping("/{idUsuario}")
-    public ResponseEntity<Void> remover(@PathVariable(name = "idUsuario")Integer idUsuario) throws RegraDeNegocioException {
+    public ResponseEntity<Void> remover(@PathVariable(name = "idUsuario") Integer idUsuario) throws RegraDeNegocioException {
         usuarioService.remover(idUsuario);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Atualizar o tipo do Usuário", description = "Atualiza o tipo do usuário no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Atualizou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "404", description = "Não encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PutMapping("/{idUsuario}/admin")
-    public ResponseEntity<Void> tornarUsuarioAdmin(@PathVariable(name = "idUsuario")Integer idUsuario) throws RegraDeNegocioException {
+    public ResponseEntity<Void> tornarUsuarioAdmin(@PathVariable(name = "idUsuario") Integer idUsuario) throws RegraDeNegocioException {
         usuarioService.remover(idUsuario);
         log.info("Usuário deletado com sucesso");
         return ResponseEntity.noContent().build();
