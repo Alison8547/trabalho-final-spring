b51ad17674d15e86216312d4db4c6111355619be
Luiz Martins <luiiz.martiins@outlook.com>
2022-11-05 04:16:36 -0300

    alterei as classes de assistidos

--- src/main/java/br/com/dbc/dbcmovies/service/AssistidosService.java
+++ src/main/java/br/com/dbc/dbcmovies/service/AssistidosService.java
@@ -1,60 +1,80 @@
-//package br.com.dbc.dbcmovies.service;
-//
-//import br.com.dbc.dbcmovies.dto.ItemEntretenimentoDto;
-//import br.com.dbc.dbcmovies.entity.ItemEntretenimentoEntity;
-//import br.com.dbc.dbcmovies.exceptions.RegraDeNegocioException;
-//import br.com.dbc.dbcmovies.repository.AssistidosRepository;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class AssistidosService {
-//
-//    private final AssistidosRepository assistidosRepository;
-//    private final UsuarioService usuarioService;
-//    private final ItemService itemService;
-//    private final ObjectMapper objectMapper;
-//
-//    public List<ItemEntretenimentoDto> listarAssistidos(Integer idUsuario) throws RegraDeNegocioException {
-//        usuarioService.findById(idUsuario);
-//
-//        List<ItemEntretenimentoEntity> resultList = null;
-//        try {
-//            resultList = assistidosRepository.listarAssistidos(idUsuario);
-//        } catch (BancoDeDadosException e) {
-//            throw new RegraDeNegocioException(e.getMessage());
-//        }
-//        resultList.forEach(item -> item.setMediaAvaliacoes(itemService.calcularAvaliacao(item.getId())));
-//
-//        return resultList.stream()
-//                .map(item -> objectMapper.convertValue(item, ItemEntretenimentoDto.class))
-//                .toList();
-//    }
-//
-//    public void deletarAssistido(Integer idItem, Integer idUsuario) throws RegraDeNegocioException {
-//        itemService.findById(idItem);
-//        usuarioService.findById(idUsuario);
-//        try {
-//            assistidosRepository.deletarAssistido(idItem, idUsuario);
-//        } catch (BancoDeDadosException e) {
-//            throw new RegraDeNegocioException(e.getMessage());
-//        }
-//    }
-//
-//    public ItemEntretenimentoDto marcarAssistido(Integer idItem, Integer idUsuario) throws RegraDeNegocioException {
-//        itemService.findById(idItem);
-//        usuarioService.findById(idUsuario);
-//
-//        try {
-//            assistidosRepository.marcarAssistido(idUsuario, idItem);
-//        } catch (BancoDeDadosException e) {
-//            throw new RegraDeNegocioException(e.getMessage());
-//        }
-//
-//        return objectMapper.convertValue(itemService.findById(idItem), ItemEntretenimentoDto.class);
-//    }
-//}
+package br.com.dbc.dbcmovies.service;
+
+import br.com.dbc.dbcmovies.dto.ItemEntretenimentoDto;
+import br.com.dbc.dbcmovies.entity.ItemEntretenimentoEntity;
+import br.com.dbc.dbcmovies.entity.UsuarioEntity;
+import br.com.dbc.dbcmovies.exceptions.RegraDeNegocioException;
+import br.com.dbc.dbcmovies.repository.AssistidosRepository;
+import br.com.dbc.dbcmovies.repository.ItemRepository;
+import br.com.dbc.dbcmovies.repository.UsuarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AssistidosService {
+
+    private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
+    private final ItemRepository itemRepository;
+    private final ItemService itemService;
+    private final AssistidosRepository assistidosRepository;
+    private final ObjectMapper objectMapper;
+
+    public List<ItemEntretenimentoDto> listarAssistidos(Integer idUsuario) throws RegraDeNegocioException {
+        UsuarioEntity usuarioEntity = usuarioService.findById(idUsuario);
+        return usuarioEntity.getItemEntretenimentos().stream()
+                .map(item -> objectMapper.convertValue(item, ItemEntretenimentoDto.class)).toList();
+    }
+
+    public ItemEntretenimentoDto marcarAssistido(Integer idItem, Integer idUsuario) throws RegraDeNegocioException {
+        ItemEntretenimentoEntity item = itemService.findById(idItem);
+        UsuarioEntity usuario = usuarioService.findById(idUsuario);
+
+        verificarItemAssistido(idItem, usuario);
+
+        usuario.getItemEntretenimentos().add(item);
+        item.getUsuarios().add(usuario);
+
+        itemRepository.save(item);
+        usuarioRepository.save(usuario);
+
+        return objectMapper.convertValue(item, ItemEntretenimentoDto.class);
+    }
+
+    public void deletarAssistido(Integer idItem, Integer idUsuario) throws RegraDeNegocioException {
+        UsuarioEntity usuario = usuarioService.findById(idUsuario);
+        ItemEntretenimentoEntity item = itemService.findById(idItem);
+
+        this.verificarUsuarioNaTabela(idUsuario);
+        verificarItemNaTabela(idItem, usuario);
+
+        item.getUsuarios().remove(usuario);
+        usuario.getItemEntretenimentos().remove(item);
+
+        usuarioRepository.save(usuario);
+        itemRepository.save(item);
+    }
+
+    public void verificarItemAssistido(Integer idItem, UsuarioEntity usuario) throws RegraDeNegocioException {
+        usuario.getItemEntretenimentos().stream()
+                .filter(item -> !(item.getIdItem() == idItem))
+                .findFirst()
+                .orElseThrow(() -> new RegraDeNegocioException("Item já está marcado como assistido para o usuário selecionado!"));
+    }
+
+    public void verificarItemNaTabela(Integer idItem, UsuarioEntity usuario) throws RegraDeNegocioException {
+        usuario.getItemEntretenimentos().stream()
+                .filter(item -> item.getIdItem() == idItem)
+                .findFirst()
+                .orElseThrow(() -> new RegraDeNegocioException("Item ainda não foi maracado como assistido para o usuário selecionado"));
+    }
+    public void verificarUsuarioNaTabela(Integer idUsuario) throws RegraDeNegocioException {
+        if(assistidosRepository.verificarUsuarioNaTabela(idUsuario) == 0) {
+            throw new RegraDeNegocioException("Usuário selecionado não existe na tebela de assistidos!");
+        }
+    }
+}
