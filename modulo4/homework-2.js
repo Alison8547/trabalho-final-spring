d7a960b27be267baf1c5b8014adab0bba6301d36
Luiz Martins <luiiz.martiins@outlook.com>
2022-11-14 18:32:43 -0300

    homework-2.js finalizado

---
+++ modulo4/homework-2.js
@@ -0,0 +1,365 @@
+db.itens.insert(
+	[
+  {
+    "tipo": "filme",
+    "nome": "Duro de Matar",
+    "genero": "acao",
+    "sinopse": "foda",
+    "anoLancamento": "1988",
+    "classificacao": 18,
+    "plataforma": "Netflix",
+    "duracao": "120",
+    "idItem": 1
+  },
+  {
+    "tipo": "filme",
+    "nome": "Titanic",
+    "genero": "Romance",
+    "sinopse": "triste",
+    "anoLancamento": "1997",
+    "classificacao": 12,
+    "plataforma": "Filmes online gratis HD",
+    "duracao": "155",
+    "idItem": 2
+  },
+  {
+    "tipo": "filme",
+    "nome": "Senhor dos aneis",
+    "genero": "Fantasia",
+    "sinopse": "muito foda",
+    "anoLancamento": "2001",
+    "classificacao": 12,
+    "plataforma": "Amazon prime",
+    "duracao": "220",
+    "idItem": 3
+  },
+  {
+    "tipo": "serie",
+    "nome": "Game of Thrones",
+    "genero": "Drama",
+    "sinopse": "Um monte gente brigando pra ver quem vai ser Rei",
+    "anoLancamento": "2011",
+    "classificacao": 16,
+    "plataforma": "HBO MAX",
+    "temporadas": 7,
+    "episodios": 73,
+    "idItem": 4
+  },
+  {
+    "tipo": "serie",
+    "nome": "The Office",
+    "genero": "Comedia",
+    "sinopse": "Mostra a rotina de um escritorio",
+    "anoLancamento": "2005",
+    "classificacao": 14,
+    "plataforma": "HBO MAX",
+    "temporadas": 9,
+    "episodios": 120,
+    "idItem": 5
+  },
+  {
+    "tipo": "serie",
+    "nome": "Breaking Bad",
+    "genero": "Drama",
+    "sinopse": "Um professor de quimica, que descobriu que tem um cancêr. E está fazendo de tudo para deixar dinheiro pra sua família",
+    "anoLancamento": "2008",
+    "classificacao": 16,
+    "plataforma": "HBO MAX",
+    "temporadas": 5,
+    "episodios": 62,
+    "idItem": 6
+  },
+  {
+    "tipo": "serie",
+    "nome": "O Exorcista",
+    "genero": "Terror",
+    "sinopse": "Dois homens muito diferentes dirigem seus esforços a um caso terrível de possessão demoníaca numa família local",
+    "anoLancamento": "2019",
+    "classificacao": 18,
+    "plataforma": "Netflix",
+    "temporadas": 2,
+    "episodios": 12,
+    "idItem": 7
+  },
+  {
+    "tipo": "serie",
+    "nome": "Supernatural",
+    "genero": "Terror",
+    "sinopse": "Os irmãos Sam e Dean Winchester encaram cenários sinistros caçando monstros",
+    "anoLancamento": "2005",
+    "classificacao": 18,
+    "plataforma": "Netflix",
+    "temporadas": 15,
+    "episodios": 200,
+    "idItem": 8
+  },
+  {
+    "tipo": "serie",
+    "nome": "A Maldição da Mansão Bly",
+    "genero": "Terror",
+    "sinopse": "Uma babá norte-americana chega a Bly Manor e começa a ver aparições em uma propriedade inglesa",
+    "anoLancamento": "2020",
+    "classificacao": 18,
+    "plataforma": "Netflix",
+    "temporadas": 1,
+    "episodios": 6,
+    "idItem": 9
+  },
+  {
+    "tipo": "filme",
+    "nome": "Logan",
+    "genero": "Acao",
+    "sinopse": "Em 2024, os mutantes estão em franco declínio, e as pessoas não sabem o real motivo.",
+    "anoLancamento": "2019",
+    "classificacao": 18,
+    "plataforma": "Cinema",
+    "duracao": "160",
+    "idItem": 10
+  },
+  {
+    "tipo": "filme",
+    "nome": "Shazam",
+    "genero": "Acao",
+    "sinopse": "Billy Batson tem apenas 14 anos de idade, mas recebeu de um antigo mago o dom de se transformar.",
+    "anoLancamento": "2019",
+    "classificacao": 16,
+    "plataforma": "Cinema",
+    "duracao": "120",
+    "idItem": 11
+  },
+  {
+    "tipo": "serie",
+    "nome": "Peaky Blinders",
+    "genero": "Drama",
+    "sinopse": "Uma notória gangue da Inglaterra de 1919 ascende no submundo liderada pelo cruel Tommy Shelby, um criminoso disposto a subir na vida a qualquer preço.",
+    "anoLancamento": "2013",
+    "classificacao": 18,
+    "plataforma": "Netflix",
+    "temporadas": 6,
+    "episodios": 32,
+    "idItem": 12
+  },
+  {
+    "tipo": "Serie",
+    "nome": "The Boys",
+    "genero": "Acao",
+    "sinopse": "A Terra é habitada por super-heróis que são um inspiração para a humanidade.",
+    "anoLancamento": "2019",
+    "classificacao": 18,
+    "plataforma": "HBO MAX",
+    "temporadas": 4,
+    "episodios": 34,
+    "idItem": 15
+  },
+  {
+    "tipo": "Serie",
+    "nome": "The Sandman",
+    "genero": "Drama",
+    "sinopse": "Após anos aprisionado, Morpheus, o Rei dos Sonhos, embarca em uma jornada entre mundos para recuperar o que lhe foi roubado e restaurar seu poder.",
+    "anoLancamento": "2022",
+    "classificacao": 17,
+    "plataforma": "Netflix",
+    "temporadas": 1,
+    "episodios": 13,
+    "idItem": 14
+  },
+  {
+    "tipo": "Serie",
+    "nome": "The Witcher",
+    "genero": "Fantasia",
+    "sinopse": "O mutante Geralt de Rívia é um caçador de monstros que luta para encontrar seu lugar.",
+    "anoLancamento": "2019",
+    "classificacao": 18,
+    "plataforma": "Netflix",
+    "temporadas": 2,
+    "episodios": 16,
+    "idItem": 16
+  },
+  {
+    "tipo": "Filme",
+    "nome": "Vingadores: Guerra Infinita",
+    "genero": "Ficcao cientifica",
+    "sinopse": "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos.",
+    "anoLancamento": "2018",
+    "classificacao": 16,
+    "plataforma": "Cinema",
+    "duracao": "170",
+    "idItem": 17
+  },
+  {
+    "tipo": "Filme",
+    "nome": "Wolverine - Imortal",
+    "genero": "Ficcao cientifica",
+    "sinopse": "Após matar Jean Grey para salvar a humanidade, Logan passou a viver na selva.",
+    "anoLancamento": "2013",
+    "classificacao": 18,
+    "plataforma": "TV",
+    "duracao": "150",
+    "idItem": 18
+  },
+  {
+    "tipo": "Filme",
+    "nome": "Transformers 4",
+    "genero": "Acao",
+    "sinopse": "Depois da batalha entre Autobots e Decepticons, que arrasou Chicago, os gigantescos robôs alienígenas desaparecem.",
+    "anoLancamento": "2014",
+    "classificacao": 16,
+    "plataforma": "TV",
+    "duracao": "120",
+    "idItem": 19
+  },
+  {
+    "tipo": "Filme",
+    "nome": "Motoqueiro Fantasma",
+    "genero": "Fantasia",
+    "sinopse": "Depois de um período recluso na Europa, Johnny Blaze embarca em uma nova missão para se livrar da maldição de ser um motoqueiro fantasma.",
+    "anoLancamento": "2011",
+    "classificacao": 18,
+    "plataforma": "TV",
+    "duracao": "125",
+    "idItem": 20
+  },
+  {
+    "tipo": "Serie",
+    "nome": "The Handmaid's Tale",
+    "genero": "Ficcao cientifica",
+    "sinopse": "Depois que um atentado terrorista tira a vida do presidente dos Estados Unidos e de grande parte dos outros políticos eleitos, uma facção católica toma o poder com o intuito de restaurar a paz.",
+    "anoLancamento": "2017",
+    "classificacao": 18,
+    "plataforma": "Amazon Prime",
+    "temporadas": 5,
+    "episodios": 34,
+    "idItem": 26
+  },
+  {
+    "tipo": "filme",
+    "nome": "O Culpado",
+    "genero": "Suspense",
+    "sinopse": "Contando com pouquíssimas pistas, um operador de chamadas de emergência tenta salvar a vida uma mulher desesperada.",
+    "anoLancamento": "2018",
+    "classificacao": 16,
+    "plataforma": "Apple TV",
+    "duracao": "130",
+    "idItem": 24
+  },
+  {
+    "tipo": "Serie",
+    "nome": "A Casa do Dragão",
+    "genero": "Drama",
+    "sinopse": "Os apoiadores de Aegon Targaryen entram em conflito com os adeptos de sua meia-irmã, Rhaenyra, pelo trono de Viserys I.",
+    "anoLancamento": "2022",
+    "classificacao": 18,
+    "plataforma": "HBO MAX",
+    "temporadas": 1,
+    "episodios": 12,
+    "idItem": 25
+  },
+  {
+    "tipo": "Serie",
+    "nome": "Moon Knight",
+    "genero": "Acao",
+    "sinopse": "Steven Grant e Marc Spector investigam mistérios dos deuses do Egito de dentro do mesmo corpo.",
+    "anoLancamento": "2020",
+    "classificacao": 18,
+    "plataforma": "Disney plus",
+    "temporadas": 1,
+    "episodios": 6,
+    "idItem": 27
+  },
+  {
+    "tipo": "Serie",
+    "nome": "Stranger Things",
+    "genero": "Misterio",
+    "sinopse": "Um grupo de amigos se envolve em uma série de eventos sobrenaturais na pacata cidade de Hawkins.",
+    "anoLancamento": "2016",
+    "classificacao": 16,
+    "plataforma": "Netflix",
+    "temporadas": 4,
+    "episodios": 32,
+    "idItem": 28
+  },
+  {
+    "tipo": "filme",
+    "nome": "Ted",
+    "genero": "Comedia",
+    "sinopse": "Quando criança, John desejou que seu ursinho Ted ganhasse vida e, surpreendentemente, foi atendido.",
+    "anoLancamento": "2012",
+    "classificacao": 16,
+    "plataforma": "Google play filmes",
+    "duracao": "120",
+    "idItem": 29
+  },
+  {
+    "tipo": "filme",
+    "nome": "Anjos da Lei",
+    "genero": "Comedia",
+    "sinopse": "enko e Schmidt estudaram juntos, mas nunca foram amigos. Anos depois, os dois se reencontram na academia de polícia e passam a se ajudar.",
+    "anoLancamento": "2012",
+    "classificacao": 16,
+    "plataforma": "Yuotube",
+    "duracao": "110",
+    "idItem": 30
+  },
+  {
+    "tipo": "filme",
+    "nome": "Norbit - Uma Comédia de Peso",
+    "genero": "Comedia",
+    "sinopse": "Desde que foi abandonado em um orfanato, Norbit sempre teve uma vida muito dura. Agora, casado com Rasputia.",
+    "anoLancamento": "2007",
+    "classificacao": 16,
+    "plataforma": "TV",
+    "duracao": "118",
+    "idItem": 31
+  },
+  {
+    "tipo": "Filme",
+    "nome": "Liga da Justiça",
+    "genero": "acao",
+    "sinopse": "Juntos, Batman e Mulher-Maravilha buscam e recrutam com agilidade um time de meta-humanos.",
+    "anoLancamento": "2021",
+    "classificacao": 16,
+    "plataforma": "TV",
+    "duracao": "180",
+    "idItem": 32
+  },
+  {
+    "tipo": "Serie",
+    "nome": "His Dark Materials",
+    "genero": "animacao",
+    "sinopse": "Durante sua busca por um amigo sequestrado, uma valente garota chamada Lyra descobre uma trama sinistra.",
+    "anoLancamento": "2019",
+    "classificacao": 16,
+    "plataforma": "HBO MAX",
+    "temporadas": 3,
+    "episodios": 18,
+    "idItem": 33
+  },
+  {
+    "tipo": "Filme",
+    "nome": "El Camino",
+    "genero": "Crime",
+    "sinopse": "Assombrado pelo passado, o fugitivo Jesse Pinkman tenta encontrar um lugar seguro para viver.",
+    "anoLancamento": "2019",
+    "classificacao": 16,
+    "plataforma": "Netflix",
+    "duracao": "120",
+    "idItem": 34
+  }
+]
+)
+
+
+db.itens.find()
+
+db.itens.find({
+	$or : [
+		{"genero" : "Terror"},
+		{"classificacao" : 18}
+	]
+})
+
+db.itens.find({
+	"plataforma" : {
+		$in : ["Netflix", "Amazon Prime"]
+	}
+})
